09d1d294b58a792b22c96f32d2cda1a0e493e23b
DjhonyOliveira <djonatandeolivera@hotmail.com>
2024-09-13 23:08:13 -0300

    final media

--- aula05/pratica 02/index.js
+++ aula05/pratica 02/index.js
@@ -1,10 +1,10 @@
 const mediaGeral   = document.getElementById('media-geral');
 const mediaMateria = document.getElementById('media-materia');
 const tabela       = document.getElementById('table');
-let mediaAlunos    = [];
 
 mediaGeral.addEventListener('click', function(){
-    let linhas       = document.querySelectorAll("tr");
+    let linhas      = document.querySelectorAll("tr");
+    let mediaAlunos = [];
 
     for(let i = 2; i <= linhas.length; i++){
         let celulas = linhas[i].querySelectorAll("td");
@@ -14,13 +14,16 @@ mediaGeral.addEventListener('click', function(){
             aluno.push(celula.textContent);
         });
 
-        mediaAlunos.push(criaEstruturaArrayAlunos(aluno));
-        
+        let dadosAluno = criaEstruturaArrayAlunos(aluno);
+
+        celulas[10].innerText = dadosAluno.media;
     }
-
-
 });
 
+/**
+ * @param {Array} aAluno 
+ * @returns {Array} - Realiza a estruturação dos dados do aluno
+ */
 function criaEstruturaArrayAlunos(aAluno){
     let notasAluno = [];
 
@@ -29,22 +32,29 @@ function criaEstruturaArrayAlunos(aAluno){
     }
 
     let aluno = {
-        nome: aAluno[0],
-        notas: notasAluno
+        nome  : aAluno[0],
+        notas : notasAluno,
+        media : calculaMedia(notasAluno)
     }
 
     return aluno;
 }
 
+/**
+ * @param {Array} aNotas 
+ * @returns {float} - Calcula a média final do aluno
+ */
 function calculaMedia(aNotas){
     let somaNotas  = 0;
     let mediaFinal = 0;
 
     aNotas.forEach(nota => {
-        somaNotas = parseFloat(nota) + parseFloat(somaNotas);
+        if(nota != '' && nota != undefined){
+            somaNotas = parseFloat(nota) + parseFloat(somaNotas);
+        }
     });
 
-    mediaFinal = somaNotas / (aNotas.length - 1);
+    mediaFinal = somaNotas / (aNotas.length - 2);
 
-    return mediaFinal;
+    return parseFloat(mediaFinal.toFixed(2));
 }
